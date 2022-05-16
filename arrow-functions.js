9bca50657f7c2fdaef72b7cd2c138a1613f295bb
LordButley <joshjwwilson@gmail.com>
2022-05-16 14:24:22 +0000

    add arrow fn with parameters

--- arrow-functions.js
+++ arrow-functions.js
@@ -11,6 +11,12 @@ let sum = addThreeNumbers(5, 2, 7);
 console.log(sum);
 // Arrow Function With Parameters
 
+const addFourNumbers = (a, b, c, d) => {
+    return a + b + c + d;
+}
+console.log(addFourNumbers);
+
+
 // Single Line Arrow Function With Parameters
 
 // Implicit Returns
